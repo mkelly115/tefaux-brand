07e5a0b53be6935eb4ace3656ea1d0dd0c124cbb
Michael <mikek222@gmail.com>
2024-09-28 13:06:06 -0400

    added shops and nav placeholders for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component.jsx'
+
+const Navigation = () => {
+  return(
+    <div>
+      <div>
+    <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+};
+
+const Shop = () => {
+ return <h1>I am the shop page</h1>
+};
 
 const App = () => {
   return (
-    <BrowserRouter>
       <Routes>
-        <Route path='/' element={< Home />} />
+      <Route path='/' element={<Navigation />} > 
+        <Route index element={<Home />} />\
+        <Route path='shop' element={<Shop />} />
+      </Route>
       </Routes>
-    </BrowserRouter>
+  
 
   )
 }
