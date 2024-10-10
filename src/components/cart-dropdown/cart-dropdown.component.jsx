354d93d28247e6e83dfa9cf6bbfbe7fe06246b3d
Michael <mikek222@gmail.com>
2024-10-10 15:11:45 -0400

    fixed shopping cart error, as well as NaN input error

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react';
+
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import { CartContext } from '../../contexts/cart.context';
 
 
 const CartDropdown = () => {
-    return(
+    const { cartItems } = useContext(CartContext);
+
+    return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                
+                {cartItems.map((item) => (
+                    <CartItem key={item.id} CartItem={item} />
+                ))}
             </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,38 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    );
+
+    if (existingCartItem) {
+        return cartItems.map((cartItem) => 
+            cartItem.id === productToAdd.id 
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+    }
+
+    return [...cartItems, { ...productToAdd, quantity: 1 }]
+};
+
 export const CartContext = createContext({
     isCartopen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => { },
+    cartItems: [],
+    addItemToCart: () => { },
 })
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
     const [isCartopen, setIsCartOpen] = useState(false);
-    const value = {isCartopen, setIsCartOpen};
+    const [cartItems, setCartItems] = useState([])
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+
+    const value = { isCartopen, setIsCartOpen, addItemToCart, cartItems };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 
