d4a45ef0fe49b4c2e6556e69865d1836205f39aa
Michael <mikek222@gmail.com>
2024-10-11 12:08:33 -0400

    completed shopping cart counter

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,15 +7,19 @@ import './cart-icon.styles.scss'
 
 const CartIcon = () => {
 
-    const { isCartopen, setIsCartOpen } = useContext(CartContext);
+    const { isCartopen, setIsCartOpen, cartCount } = useContext(CartContext);
     const toggleIsCartOpen = () => setIsCartOpen(!isCartopen)
 
     return(
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     )
 }
 
 export default CartIcon
+
+// click add to shopping cart
+
+// item will add +1 to shopping cart counter
