9edeb7ddc2a792787af57b4df3369271c85ddb53
Michael <mikek222@gmail.com>
2024-10-16 15:54:00 -0400

    funtioning routing for each category page

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
-import { useContext, useState, useEffect } from 'react';
+
+import { useContext, useState, useEffect, Fragment } from 'react';
 import ProductCard from '../../components/product-card/product-card.component';
 import { CategoriesContext } from '../../contexts/categories.context';
 
@@ -15,12 +16,15 @@ const Category = () => {
 
     }, [category, categoriesMap])
 
-    return(
-        <div className='category-container'>
-            {products &&
-                products.map((product) => <ProductCard key={product.id} product={product} />)
-            }
-        </div>
+    return (
+        <Fragment>
+            <h2 className='category-title'>{category.toUpperCase()}</h2>
+            <div className='category-container'>
+                {products &&
+                    products.map((product) => <ProductCard key={product.id} product={product} />)
+                }
+            </div>
+        </Fragment>
     )
 };
 
