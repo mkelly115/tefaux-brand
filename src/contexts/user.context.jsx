b1e84ad9a2c23a5db103c464421bb12225511870
Michael <mikek222@gmail.com>
2024-11-04 13:52:10 -0500

    first attempt at moving from state to redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
@@ -6,20 +6,49 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return{
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = { currentUser, setCurrentUser};
+    // const [currentUser, setCurrentUser] = useState(null);
+    const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+    const setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    }
+
+
+    const value = { currentUser, setCurrentUser };
 
     useEffect(() => {
-       const unsubscribe = onAuthStateChangedListener((user) => {
-        console.log(user);
-        if(user){
-            createUserDocumentFromAuth(user)
-        }
-        setCurrentUser(user);
-       });
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            console.log(user);
+            if (user) {
+                createUserDocumentFromAuth(user)
+            }
+            setCurrentUser(user);
+        });
 
-       return unsubscribe
+        return unsubscribe
     }, [])
 
     return <UserContext.Provider value={value}>
