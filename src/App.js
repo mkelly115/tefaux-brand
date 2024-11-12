771843b7ceb65eb3f2d617d28cce062bffb200f9
Michael <mikek222@gmail.com>
2024-11-12 17:52:27 -0500

    redux added, user context swapped to redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Home from './routes/home/home.component.jsx'
 import Navigation from './routes/navigation/navigation.component.jsx';
 import Authentication from './routes/authentication/authentication.component.jsx';
 import Shop from './routes/shop/shop.component.jsx';
 import Checkout from './routes/checkout/checkout.component.jsx';
+import { useEffect } from "react";
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from './utils/firebase/firebase.utils.js';
+import { setCurrentUser } from './store/user/user.action.js';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+        console.log(user);
+        if (user) {
+            createUserDocumentFromAuth(user)
+        }
+        dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe
+}, [])
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
