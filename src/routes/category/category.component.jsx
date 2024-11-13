55e958cfe0c997b3dc8f05bd336bc590acc399f2
Michael <mikek222@gmail.com>
2024-11-13 16:44:35 -0500

    redux conversion complete for user and categories/categoriesPreview

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,14 +1,14 @@
 import { useParams } from 'react-router-dom'
-
-import { useContext, useState, useEffect, Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { useState, useEffect, Fragment } from 'react';
 import ProductCard from '../../components/product-card/product-card.component';
-import { CategoriesContext } from '../../contexts/categories.context';
+import { selectCategoriesMap } from '../../store/categories/category.selector.js';
 
 import { CategoryContainer, Title } from './category.styles.jsx'
 
 const Category = () => {
     const { category } = useParams();
-    const { categoriesMap } = useContext(CategoriesContext);
+    const categoriesMap = useSelector(selectCategoriesMap);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
-
+import { useEffect } from 'react';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { setCategoriesMap } from '../../store/categories/category.action'
+import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
@@ -7,6 +10,16 @@ import '../shop/shop.styles.scss'
 
 
 const Shop = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments('categories');
+            dispatch(setCategoriesMap(categoryMap));
+        }
+        getCategoriesMap();
+    }, [dispatch])
 
     return (
         <Routes>
