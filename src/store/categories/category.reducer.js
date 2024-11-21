73f405952277fddbc3404d7f1c94d5e7ec92d74b
Michael <mikek222@gmail.com>
2024-11-21 14:40:04 -0500

    thunk addition and early migration

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -2,7 +2,9 @@ import { CATEGORIES_ACTION_TYPE } from "./category.types";
 
 
 export const CATEGORIES_INITIAL_STATE = {
-    categories: []
+    categories: [],
+    isLoading: false,
+    error: null,
 }
 
 export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {}) => {
