a1155e18e286640c88a9207e4d26fb5c546a4fa0
Michael <mikek222@gmail.com>
2024-12-09 17:17:08 -0500

    changes in payment css

--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -3,18 +3,31 @@ import Button from "../button/button.component";
 
 export const PaymentFormContainer = styled.div`
 height: 300px;
-display: flex
+display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: center
+
+    @media (max-width: 768px) {
+      display: flex;
+        align-items: center;
+  
+    }
 `
 
 export const FormContainer = styled.form`
 height:100px;
 min-width: 400px;
+    @media (max-width: 768px) {
+    min-width: 100px;
+  }
 `
 
 export const PaymentButton = styled(Button)`
 margin-left: auto;
 margin-top: 30px;
+    @media (max-width: 768px) {
+    margin: auto;
+    padding: 20px;
+  }
 `
